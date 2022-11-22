af041d092f80409abdcb6e97b096bb37b7c8e0f2
ibrahim <123ibraahimm@gmail.com>
2022-11-22 10:47:35 +0700

    baru

--- database/migrations/2022_08_08_065540_create_contact_table.php
+++ database/migrations/2022_08_08_065540_create_contact_table.php
@@ -15,14 +15,14 @@ return new class extends Migration
     {
         Schema::create('contact', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('id_siswa')->unsigned();
+            $table->unsignedBigInteger('siswa_id')->unsigned();
             $table->foreign('siswa_id')->references('id')->on('siswa')
-                ->onDelete('cascode')
-                ->onUpdate('cascode');
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
             $table->BigInteger('jenis_contact_id')->unsigned();
             $table->foreign('jenis_contact_id')->references('id')->on('jenis_contact')
-                ->onDelete('cascode')
-                ->onUpdate('cascode');
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
             $table->char('deskripsi');
             $table->timestamps();
         });
